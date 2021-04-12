8f5228eb9251ccd4eeaee6d4bb5a68789c51285e
Andrei Portales <andreifan123@gmail.com>
2021-04-11 18:10:16 -0600

    antes de scroll

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -5,22 +5,50 @@ import rightArrow from "../../assets/icons/right-arrow.svg";
 import leftArrow from "../../assets/icons/left-arrow.svg";
 
 class MovieList extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.container = null;
+  }
+
+  leftScroll() {
+   
+  }
+
+  rightScroll() {
+    
+   
+  }
+
   render() {
     return (
       <div className="main">
         <div className="title">{this.props.section.title}</div>
         <div className="list-main">
-          {/* <div className="left-button">
-            <img src={leftArrow}></img>
-    </div>*/}
-          <div className="right-button">
-            <img src={rightArrow}></img>
-          </div>
-          <div className="list">
+          <div
+            className="list"
+            ref={(node) => {
+              this.container = node;
+            }}
+          >
             <div className="initial-space">.........</div>
+
             {this.props.section.movies.map((e) => {
-              return <MovieListItem key={`${e.title}-${this.props.title}`} movie={e} />;
+              return (
+                <MovieListItem
+                  key={`${e.title}-${this.props.title}`}
+                  movie={e}
+                />
+              );
             })}
+          </div>
+
+          <div className="left-button" onClick={this.leftScroll.bind(this)}>
+            <img src={leftArrow}></img>
+          </div>
+
+          <div className="right-button" onClick={this.rightScroll.bind(this)}>
+            <img src={rightArrow}></img>
           </div>
         </div>
       </div>
